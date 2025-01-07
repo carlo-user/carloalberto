5d7ca11a55e442e2effcaddd9de99616b865af29
carlo-user <128100807+carlo-user@users.noreply.github.com>
2025-01-07 18:22:25 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,28 @@
+{
+    "user1": {
+        "name": "Carlo Alberto Lisa",
+        "images": [
+            {
+                "src": "download1.jpg",
+                "description": "Questa è la descrizione dell'immagine 1."
+            },
+            {
+                "src": "download2.jpg",
+                "description": "Questa è la descrizione dell'immagine 2."
+            }
+        ]
+    },
+    "user2": {
+        "name": "Maria Rossi",
+        "images": [
+            {
+                "src": "image1.jpg",
+                "description": "Descrizione dell'immagine 1 per Maria."
+            },
+            {
+                "src": "image2.jpg",
+                "description": "Descrizione dell'immagine 2 per Maria."
+            }
+        ]
+    }
+}

1a62676a5516cf69776bba47104ee5c2ee7826ac
carlo-user <128100807+carlo-user@users.noreply.github.com>
2025-02-01 16:11:05 +0100

    Update data.js

--- data.js
+++ data.js
@@ -16,7 +16,7 @@
         "name": "Maria Rossi",
         "images": [
             {
-                "src": "image1.jpg",
+                "src": "asset/maria/download.jpg",
                 "description": "Descrizione dell'immagine 1 per Maria."
             },
             {

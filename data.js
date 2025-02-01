004a6b6a19bccd9d61184d56acb63b5b06817f8c
carlo-user <128100807+carlo-user@users.noreply.github.com>
2025-02-01 16:19:08 +0100

    Update data.js

--- data.js
+++ data.js
@@ -6,10 +6,6 @@
                 "src": "asset/carlo/download.jpg",
                 "description": "Questa è la descrizione dell'immagine 1."
             },
-            {
-                "src": "download2.jpg",
-                "description": "Questa è la descrizione dell'immagine 2."
-            }
         ]
     },
     "user2": {
@@ -19,10 +15,6 @@
                 "src": "asset/maria/download.jpg",
                 "description": "Descrizione dell'immagine 1 per Maria."
             },
-            {
-                "src": "image2.jpg",
-                "description": "Descrizione dell'immagine 2 per Maria."
-            }
         ]
     }
 }

ceff1e2f618fcba828f75430ca5daca19bdc1072
carlo-user <128100807+carlo-user@users.noreply.github.com>
2025-02-01 16:16:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const userData = {
         "name": "Maria Rossi",
         "images": [
             {
-                "src": "image1.jpg",
+                "src": "asset/maria/download.jpg",
                 "description": "Descrizione dell'immagine 1 per Maria."
             },
             {
